3ecbde9ef56af3a78164e78589bbcf7c5d50e0f2
Luisr0h <l.r.h.araya@gmail.com>
2026-07-01 18:51:21 +0200

    Feat[EnsambliaAPI] Added generoMusicalModel, generoMusicalController, generoMusicalRoutes, Updated app.js CLOSES #1

--- src/app.js
+++ src/app.js
@@ -1,28 +1,31 @@
 import dotenv from 'dotenv'
-dotenv.config()
-
-import express from 'express'
 import cors from 'cors'
-
+import express from 'express'
+import authRoutes from './routes/authRoutes.js'
 import instrumentoRoutes from './routes/instrumentoRoutes.js'
 import anuncioRoutes from './routes/anuncioRoutes.js'
 import chatRoutes from './routes/chatRoutes.js'
 import ciudadRoutes from './routes/ciudadRoutes.js'
 import comarcaRoutes from './routes/comarcaRoutes.js'
 import comentarioRoutes from './routes/comentarioRoutes.js'
+import generoMusicalRoutes from './routes/generoMusicalRoutes.js'
 
+
+dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
 
+app.use("/api/auth", authRoutes);
 app.use('/api/instrumentos', instrumentoRoutes)
 app.use('/api/anuncios', anuncioRoutes)
 app.use('/api/chats', chatRoutes)
 app.use('/api/ciudades', ciudadRoutes)
 app.use('/api/comarcas', comarcaRoutes)
 app.use('/api/comentarios', comentarioRoutes)
+app.use('/api/genero_musical', generoMusicalRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
