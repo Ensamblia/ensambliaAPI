fb37da21cff41cb694277008df6daa3444911655
Marga-codes <mmbande@00e04c56af59.localdomain>
2026-07-14 21:45:29 +0200

    Feat[ensambliaAPI] Added perfilInstrumentoModel-perfilInstrumentoController-perfilInstrumentoRoutes Closes #12

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ import perfilRoutes from './routes/perfilRoutes.js'
 import perfilChatRoutes from './routes/perfilChatRoutes.js'
 import perfilGeneroMusicalRoutes from './routes/perfilGeneroMusicalRoutes.js'
 import generoMusicalRoutes from './routes/generoMusicalRoutes.js'
+import perfilInstrumentoRoutes from './routes/perfilInstrumentoRoutes.js'
 
 
 dotenv.config()
@@ -38,6 +39,7 @@ app.use('/api/perfiles', perfilRoutes)
 app.use('/api/perfil-chats', perfilChatRoutes)
 app.use('/api/perfil-genero-musicales', perfilGeneroMusicalRoutes)
 app.use('/api/genero_musical', generoMusicalRoutes)
+app.use('/api/perfil-instrumentos', perfilInstrumentoRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
---
+++ src/controllers/perfilInstrumentoController.js
@@ -0,0 +1,154 @@
+import perfilInstrumentoModel from '../models/perfilInstrumentoModel.js'
+
+const getPerfilInstrumentos = async (req, res) => {
+    try {
+        const data = await perfilInstrumentoModel.getPerfilInstrumentos()
+        if (data.length === 0) {
+            return res.status(404).json({
+                error: "Nothing found"
+            })
+        }
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json({
+            name: error.name,
+            message: error.message,
+            code: error.code,
+            detail: error.detail,
+            hint: error.hint,
+            position: error.position
+        })
+    }
+}
+
+const getById = async (req, res) => {
+    try {
+        const { perfil_id, instrumento_id } = req.params
+        const data = await perfilInstrumentoModel.getById(perfil_id, instrumento_id)
+        if (!data) {
+            return res.status(404).json({
+                error: `Nothing found for perfil_id: ${perfil_id} and instrumento_id: ${instrumento_id}`
+            });
+        }
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json({
+            name: error.name,
+            message: error.message,
+            code: error.code,
+            detail: error.detail,
+            hint: error.hint,
+            position: error.position
+        })
+    }
+}
+
+const getByPerfilId = async (req, res) => {
+    try {
+        const { perfil_id } = req.query
+        const data = await perfilInstrumentoModel.getByPerfilId(perfil_id)
+        if (data.length === 0) {
+            return res.status(404).json({
+                error: "Nothing found"
+            })
+        }
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json({
+            name: error.name,
+            message: error.message,
+            code: error.code,
+            detail: error.detail,
+            hint: error.hint,
+            position: error.position
+        })
+    }
+}
+
+const getByInstrumentoId = async (req, res) => {
+    try {
+        const { instrumento_id } = req.query
+        const data = await perfilInstrumentoModel.getByInstrumentoId(instrumento_id)
+        if (data.length === 0) {
+            return res.status(404).json({
+                error: "Nothing found"
+            })
+        }
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json({
+            name: error.name,
+            message: error.message,
+            code: error.code,
+            detail: error.detail,
+            hint: error.hint,
+            position: error.position
+        })
+    }
+}
+
+const createPerfilInstrumento = async (req, res) => {
+    try {
+        const { perfil_id, instrumento_id } = req.body
+
+        if (perfil_id === undefined || !Number.isInteger(perfil_id)) {
+            return res.status(400).json({
+                error: "perfil_id es un campo obligatorio y debe ser un entero"
+            })
+        }
+
+        if (instrumento_id === undefined || !Number.isInteger(instrumento_id)) {
+            return res.status(400).json({
+                error: "instrumento_id es un campo obligatorio y debe ser un entero"
+            })
+        }
+
+        const data = await perfilInstrumentoModel.createPerfilInstrumento(perfil_id, instrumento_id)
+        if (!data) {
+            return res.status(404).json({
+                error: "Perfil_instrumento no añadido correctamente"
+            });
+        }
+        res.status(201).json(data)
+    } catch (error) {
+        res.status(500).json({
+            name: error.name,
+            message: error.message,
+            code: error.code,
+            detail: error.detail,
+            hint: error.hint,
+            position: error.position
+        })
+    }
+}
+
+const deletePerfilInstrumento = async (req, res) => {
+    try {
+        const { perfil_id, instrumento_id } = req.params
+        const data = await perfilInstrumentoModel.deletePerfilInstrumento(perfil_id, instrumento_id)
+        if (!data) {
+            return res.status(404).json({
+                error: "Perfil_instrumento no encontrado"
+            });
+        }
+        res.json(data)
+    } catch (error) {
+        res.status(500).json({
+            name: error.name,
+            message: error.message,
+            code: error.code,
+            detail: error.detail,
+            hint: error.hint,
+            position: error.position
+        })
+    }
+}
+
+export default {
+    getPerfilInstrumentos,
+    getById,
+    getByPerfilId,
+    getByInstrumentoId,
+    createPerfilInstrumento,
+    deletePerfilInstrumento
+}
